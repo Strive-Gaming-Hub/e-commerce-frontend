6cab57289d93a3c3d720f899276be2cd51a453c5
Sparsh47 <sparshshandilya123@gmail.com>
2025-02-21 14:09:42 +0530

    fixed carousel loader position and increased padding in limited offer banner

--- src/components/LimitedOffer/LimitedOffer.jsx
+++ src/components/LimitedOffer/LimitedOffer.jsx
@@ -3,7 +3,7 @@ import Timer from "@/components/LimitedOffer/Timer";
 
 export default function LimitedOffer() {
     return (
-        <div className="w-full px-6 py-3 bg-[#474f45] text-white flex items-center justify-between">
+        <div className="w-full px-6 py-5 bg-[#474f45] text-white flex items-center justify-between">
             <div className="flex items-center gap-5">
                 <p className="font-semibold">UP TO 40% OFF FASHION FINDS—LIMITED TIME ONLY!</p>
                 <Link href="#" className="underline uppercase text-xs font-medium">

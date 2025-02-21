42a9b0e3d8a0930ff51063063f649090a16f01de
Sparsh47 <sparshshandilya123@gmail.com>
2025-02-21 16:00:23 +0530

    created offer marquee

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Carousel from "@/components/Carousel/HeroCarousel/Carousel";
 import LimitedOffer from "@/components/LimitedOffer/LimitedOffer";
 import CategoryContainer from "@/components/Categories/CategoryContainer";
+import Marquee from "@/components/Marquee";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
         <Carousel />
           <LimitedOffer />
           <CategoryContainer />
+          <Marquee />
       </div>
   );
 }

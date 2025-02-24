88b0bd6c61c41c018e58c7bce39b95974a26eaf0
Sparsh47 <sparshshandilya123@gmail.com>
2025-02-24 14:39:13 +0530

    made featured model section responsive

--- src/components/FeaturedModels/FeaturedModelCards.jsx
+++ src/components/FeaturedModels/FeaturedModelCards.jsx
@@ -3,7 +3,7 @@ export default function FeaturedModelCards({featuredData}) {
     console.log(featuredData);
 
     return (
-        <div className="w-full h-[600px] relative">
+        <div className="w-full max-w-lg h-[450px] md:h-[600px] relative">
             <div className="w-full h-full bg-gray-300"></div>
             <div className="absolute bottom-5 text-white z-20 uppercase px-5 flex flex-col gap-2">
                 <p className="text-xs font-medium">{featuredData.highlight}</p>
--- src/components/FeaturedModels/FeaturedModelsList.jsx
+++ src/components/FeaturedModels/FeaturedModelsList.jsx
@@ -17,10 +17,12 @@ const featuredModelData = [
 
 export default function FeaturedModelsList() {
     return (
-        <div className="w-full flex items-center justify-center gap-5 px-8">
-            {featuredModelData.map((feature, index) => (
-                <FeaturedModelCards key={index} featuredData={feature} />
-            ))}
+        <div className="w-full flex items-center justify-center px-8">
+            <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
+                {featuredModelData.map((feature, index) => (
+                    <FeaturedModelCards key={index} featuredData={feature} />
+                ))}
+            </div>
         </div>
     )
 }
